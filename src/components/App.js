e4c917dbbd66d9bca430e170bf3a8f3eb488820b
Bakeloh <bakelohnoel@gmail.com>
2023-12-21 12:06:36 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -17,3 +17,9 @@ function App() {
 }
 
 export default App;
+
+
+// There are four components in this application,
+//  all located in the components folder. 
+//  Use the default export syntax to export the <About>,
+//   <Home>, and <NavBar> components, and import them all in the <App> component.
